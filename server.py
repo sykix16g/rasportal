1906fc80478a4dd4dd7678acd629ecfd8c3151a6
sykx <sy.p.sxxxx@gmail.com>
2020-05-09 19:41:33 +0900

    サイトのレンダリングにJinja2を採用。ディレクトリ構成を変更した

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*-
+from jinja2 import Environment, FileSystemLoader
 from flask import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    return render_template("index.tpl.html")
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0')
